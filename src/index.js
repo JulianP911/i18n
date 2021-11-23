1edb541220ea920d8e18999cd3cf40801240f96b
JulianP911 <j.padilla@uniandes.edu.co>
2021-11-23 13:48:32 -0500

    Retos de i18n done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
 
 import JobsList from "./components/jobslist";
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const language = window.navigator.language || navigator.browserLanguage;
+
+const selectMessages = () => {
+    if (language === "en") {
+        return localeEnMessages;
+    } else {
+        return localeEsMessages;
+    }
+}
+
+ReactDOM.render(
+    <IntlProvider locale={language} messages={selectMessages()}>
+        <JobsList />
+    </IntlProvider>, 
+    document.getElementById("root")
+);
